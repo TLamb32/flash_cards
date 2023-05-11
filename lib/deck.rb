8c0f8219a1f646eafda3061de75e37372a0e8bfb
Terry Lamb <terrylamb@gmail.com>
2023-05-10 21:16:15 -0400

    add Deck spec and class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,22 @@
 class Deck
-    
+    attr_reader :cards
+    def initialize(cards)
+        @cards = cards
+    end
+    def cards_in_category(category)
+        @cards.find_all do |card|
+            card.category == category
+        end
+    end
 end
+# # def cards_in_category(category)
+# #     card_category = []
+# #     @cards.each do |card|
+# #         if card.category == category
+# #             card_category << card
+#         end
+
+#     end
+#     card_category
+    
+# end
